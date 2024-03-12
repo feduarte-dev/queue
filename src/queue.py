1f70da40daf983d9b4e265978a477b025bca27f3
Felipe Duarte <feduarte.dev@gmail.com>
2024-03-12 13:55:48 -0300

    exercicio pronto

--- src/queue.py
+++ src/queue.py
@@ -1,5 +1,18 @@
-# from src.linked_list import LinkedList
+from src.linked_list import LinkedList
 
 
 class Queue:
-    raise NotImplementedError
+    def __init__(self):
+        self.__data = LinkedList()
+
+    def enqueue(self, value):
+        self.__data.insert_last(value)
+
+    def dequeue(self):
+        return self.__data.remove_first()
+
+    def peek(self):
+        return self.__data.get_element_at(0)
+
+    def is_empty(self):
+        return not len(self.__data)
